3823f63bc2524435618812951c12472d95f60a5a
Nasra  Aden <nasraaden@Nasras-MBP.hsd1.or.comcast.net>
2020-05-19 09:48:49 -0700

    day 1

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,21 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.pc = 0
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        self.ram[address] = value
 
     def load(self):
         """Load a program into memory."""
@@ -15,28 +24,38 @@ class CPU:
         address = 0
 
         # For now, we've just hardcoded a program:
+        LDI = 0b10000010
+        PRN = 0b01000111
+        HLT = 0b00000001
+        MUL = 0b10100010
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            # From print8.ls8
+            LDI,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            LDI,  # LDI R1,9
+            0b00000001,
+            0b00001001,
+            MUL,  # MUL R0,R1
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,
+            PRN,  # PRN R0
+            0b00000000,
+            HLT,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +67,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +81,36 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        LDI = 0b10000010
+        PRN = 0b01000111
+        HLT = 0b00000001
+        MUL = 0b10100010
+
+        halted = False
+
+        while not halted:
+            instruction = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if instruction == LDI:
+                # Set the value of a register to an integer
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif instruction == PRN:
+                # Print numeric value stored in the given register.
+                # Print to the console the decimal integer value that is stored in the given register.
+                print(self.reg[operand_a])
+                self.pc += 2
+            elif instruction == MUL:
+                # Multiply the values in two registers together and store the result in registerA.
+                result = self.reg[operand_a] * self.reg[operand_b]
+                print(result)
+                self.pc += 3
+            elif instruction == HLT:
+                # Halt the CPU (and exit the emulator).
+                halted = True
+            else:
+                print(
+                    f'Unknown instruction {instruction} at address {self.pc}.')
+                sys.exit(1)
